7d34eed6cce10a69af5c55bd7f73ec5f14e53389
Ignacio Muñoz <munoz.ignacioagustin@gmail.com>
2023-06-10 20:22:21 -0300

    modif itemlistcontainer para poder ser utilizado en las categorias, sin necesitar categorylistcontainer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,24 @@
+// App.jsx
 import { Branding } from './skel/Branding/Branding'
 import { NavBar } from './skel/NavBar/NavBar'
 import { ShoppingCart } from './skel/ShoppingCart/ShoppingCart'
 import { ItemListContainer } from './skel/Items/ItemListContainer/ItemListContainer'
-import { CategoryListContainer } from './skel/Items/CategoryListContainer/CategoryListContainer'
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <Link to={`/`}><Branding/></Link>
-        <NavBar />
-        <Link to={`/cart`}><ShoppingCart /></Link>
-        <Routes>
-          <Route exact path='/' element={<ItemListContainer />}/>
-          <Route exact path='/category/:category' element={<CategoryListContainer />}/>
-        </Routes>
-      </Router>
-        
-    </div>
-  )
+    return (
+        <div className='App'>
+            <Router>
+                <Link to={`/`}><Branding/></Link>
+                <NavBar />
+                <Link to={`/cart`}><ShoppingCart /></Link>
+                <Routes>
+                    <Route exact path='/' element={<ItemListContainer />} />
+                    <Route exact path='/category/:category' element={<ItemListContainer />} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App
--- vite-project/src/skel/Items/ItemListContainer/ItemListContainer.jsx
+++ vite-project/src/skel/Items/ItemListContainer/ItemListContainer.jsx
@@ -1,25 +1,29 @@
 import { useState, useEffect, useCallback } from "react";
+import { useParams } from 'react-router-dom';
 import ItemCard from "../ItemCard/ItemCard";
 import '../Items.css'
+
 export const ItemListContainer = () => {
+    const { category } = useParams();
     const [products, setProducts] = useState([]);
 
     const fetchData = useCallback(async () => {
-        const response = await fetch('https://dummyjson.com/products');
+        let url = 'https://dummyjson.com/products';
+        if (category) {
+            url += `/category/${category}`;
+        }
+        const response = await fetch(url);
         const data = await response.json();
         setProducts(data);
-    }, []);
+    }, [category]);
 
-    useEffect(() => {fetchData();}, [fetchData]);
+    useEffect(() => { fetchData(); }, [fetchData]);
     
     return (
         <div className="cards">
-            {products.products?.map((product) => {
-                return (
-                    <ItemCard key={product.id} item={product}/>
-                )
-            })
-        }
+            {products.products?.map((product) => (
+                <ItemCard key={product.id} item={product}/>
+            ))}
         </div>
     )
 }
