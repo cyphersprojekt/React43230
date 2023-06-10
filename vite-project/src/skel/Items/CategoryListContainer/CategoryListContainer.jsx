5ac26e3fc2b0b8fbc0ce9f5f48dc0fc37acf6992
Ignacio Muñoz <munoz.ignacioagustin@gmail.com>
2023-06-10 19:49:25 -0300

    fix al error mas boludo de la faz de la tierra

--- vite-project/src/skel/Items/CategoryListContainer/CategoryListContainer.jsx
+++ vite-project/src/skel/Items/CategoryListContainer/CategoryListContainer.jsx
@@ -1,16 +1,18 @@
+//CategoryListContainer.jsx
 import { useState, useEffect, useCallback } from "react";
 import { useParams } from 'react-router-dom';
 import ItemCard from "../ItemCard/ItemCard";
 import '../Items.css'
+
 export const CategoryListContainer = () => {
     const { category } = useParams();
     const [products, setProducts] = useState([]);
 
     const fetchData = useCallback(async () => {
-        const response = await fetch('https://dummyjson.com/products/category/'+category);
+        const response = await fetch('https://dummyjson.com/products/category/' + category);
         const data = await response.json();
         setProducts(data);
-    }, []);
+    }, [category]);
 
     useEffect(() => {fetchData();}, [fetchData]);
     
@@ -20,8 +22,7 @@ export const CategoryListContainer = () => {
                 return (
                     <ItemCard key={product.id} item={product}/>
                 )
-            })
-        }
+            })}
         </div>
     )
 }
